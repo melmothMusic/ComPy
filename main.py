e6751e91fd2d4642c126667813f197da0a11b8c5
Melmoth <music.melmoth@gmail.com>
2020-10-14 09:24:13 +0200

    Creación de primer archivo con un Objeto

---
+++ main.py
@@ -0,0 +1,25 @@
+from datetime import date
+from datetime import datetime
+
+
+class Persona:
+    def __init__(self,nombre, apellido, fechaNacimiento, altura, peso, tez, colorPelo, velloFacial, profesion, estudios, hobbies):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.fechaNacimiento = fechaNacimiento
+        self.edad = datetime.now().year - fechaNacimiento
+
+        self.altura = altura
+        self.peso = peso
+        self.tez = tez
+        self.colorPelo = colorPelo
+        self.velloFacial = velloFacial
+
+        self.profesion = profesion
+        self.estudios = estudios
+        self.hobbies = hobbies
+
+Urruti = Persona('Pablo','Urruti',1995,1.8,70,'blanco','castaño claro', 'barba ligera', 'Desarrollador en Ansis','Ingeniero industrial', ['skate','futbol','slack-line','escalada','series','python'])
+
+
+print(f"\n----------------\n{Urruti.nombre} {Urruti.apellido} tiene {Urruti.edad} años.\n----------------\n")
